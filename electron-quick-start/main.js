9463a1794413ef46e775d5a6f57440b0f9d62253
xu-zhiyong <alux@sina.com>
2018-12-18 09:44:43 +0800

    修改echarts引入地点到index

--- electron-quick-start/main.js
+++ electron-quick-start/main.js
@@ -1,7 +1,8 @@
-
+// const echarts = require('./echarts')
 
 const electron = require('electron')
 // Module to control application life.
+
 const app = electron.app
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
